0240942ae390802c1282fbb19b636d4716b9c1ac
znataniel <znataniel@protonmail.com>
2024-02-25 15:31:06 -0300

    add report

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+def main():
+    path_to_book = "books/frankenstein.txt"
+    book_in_str = read_book_to_str(path_to_book)
+    wc = word_count(book_in_str)
+    lc_dict = letter_count(book_in_str)
+    print(f"--- Begin report of {path_to_book} ---")
+    print(f"{wc} words found in the document.")
+    print("\n")
+
+    def sort_on(dict):
+        return dict["num"]
+
+    letters = []
+    for l in lc_dict:
+        if (l.isalpha()):
+            letters.append({"name": l, "num": lc_dict[l]})
+    letters.sort(reverse=True, key=sort_on)
+    for l in letters:
+        letter = l["name"]
+        num = l["num"]
+        print(f"Letter {letter} was found {num} times.")
+
+
 def letter_count(book_str):
     letters = {}
     book_str = book_str.lower()
@@ -18,14 +41,6 @@ def word_count(book_str):
 def read_book_to_str(book_path):
     with open(book_path) as book:
         return book.read()
-
-
-def main():
-    book_in_str = read_book_to_str("./books/frankenstein.txt")
-    wc = word_count(book_in_str)
-    print(f"Words in book: {wc}")
-    lc_dict = letter_count(book_in_str)
-    print(lc_dict)
 
 
 main()
